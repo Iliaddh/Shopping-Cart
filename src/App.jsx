03f7a11bf4f1497f46e3e987aa3aa1592f857749
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-04-26 10:56:00 -0400

    Router set Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react' 
 import './App.css'
+import { Routes, Route , Navigate} from 'react-router-dom'
+import ProductsPage from './pages/ProductsPage'
 
 function App() { 
 
   return (
-    <>
-     
-    </>
+    <Routes>
+      <Route index element={<Navigate to="/products" replace/>} />
+      <Route path="/products" element={<ProductsPage/>}/>
+
+    </Routes>
   )
 }
 
