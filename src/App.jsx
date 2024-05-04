d3eacc0486181050652155e3ce7ddb5cbe5434c1
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-05-03 22:17:14 -0400

    Cart,adding and deleteing product functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,26 @@ import './App.css'
 import { Routes, Route , Navigate} from 'react-router-dom'
 import ProductsPage from './pages/ProductsPage'
 import ProductsProvider from './context/ProductContext'
+import CartProvider from './context/CartContext'
+import DetailsPage from "../src/pages/DetailsPage"
+import CheckoutPage from "../src/pages/CheckoutPage"
+import PageNotFound from "../src/pages/PageNotFound"
 
 function App() { 
 
   return (
+   <CartProvider>
     <ProductsProvider>
       <Routes>
         <Route index element={<Navigate to="/products" replace/>} />
         <Route path="/products" element={<ProductsPage/>}/>
+        <Route path="/products/:id" element={<DetailsPage/>}/>
+        <Route path="/checkout" element={<CheckoutPage/>}/>
+        <Route path="/*" element={<PageNotFound/>}/>
 
       </Routes>
     </ProductsProvider>
+    </CartProvider>  
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import {TbListDetails, TbShoppingBagCheck} from "react-icons/tb";
+import { MdDeleteOutline } from 'react-icons/md';
+
 import { ShortenText } from '../helpers/helper';
 import styles from "./Card.module.css"
+import { useCart } from '../context/CartContext';
+import { productQuantity } from "../helpers/helper";
 
 function Card({data}) {
     const {id, title , image, price} = data;
 
+   const [state, dispatch] =  useCart();
+    const quantity = productQuantity(state, id);
+    console.log(quantity)
+
+   const clickHandler =(type) => {
+    dispatch({type, payload: data})
+   }
   return (
     <div className={styles.card}>
         <img src={image} alt={title}  />
@@ -18,9 +29,18 @@ function Card({data}) {
             </Link>
            
                 <div >
-                    <button>
+                    {quantity === 1 && (<button onClick={()=>clickHandler("REMOVE_ITEM")}>
+                        <MdDeleteOutline/>
+                    </button>)}
+                    {quantity >1 && ( <button onClick={()=>clickHandler("DECREASE")}>-</button>)}
+                    {!!quantity && <span>{quantity}</span>}
+                    {quantity === 0 ? (<button onClick={()=>clickHandler("ADD_ITEM")}>
                         <TbShoppingBagCheck/>
-                    </button>
+                    </button>) : (<button onClick={()=>clickHandler("INCREASE")}>+</button>)}
+                    
+                    
+                    
+                    
                 </div>
            
         </div>
