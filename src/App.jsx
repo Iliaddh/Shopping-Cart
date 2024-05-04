ae2e23622f2ee3191852a9d2f329b17c993d8e4a
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-05-03 22:44:33 -0400

    Layout created

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,27 @@ import CartProvider from './context/CartContext'
 import DetailsPage from "../src/pages/DetailsPage"
 import CheckoutPage from "../src/pages/CheckoutPage"
 import PageNotFound from "../src/pages/PageNotFound"
+import Layout from './layout/Layout'
 
 function App() { 
 
   return (
-   <CartProvider>
-    <ProductsProvider>
-      <Routes>
-        <Route index element={<Navigate to="/products" replace/>} />
-        <Route path="/products" element={<ProductsPage/>}/>
-        <Route path="/products/:id" element={<DetailsPage/>}/>
-        <Route path="/checkout" element={<CheckoutPage/>}/>
-        <Route path="/*" element={<PageNotFound/>}/>
-
-      </Routes>
-    </ProductsProvider>
-    </CartProvider>  
+    
+      <CartProvider>
+        <ProductsProvider>
+          <Layout>
+            <Routes>
+              <Route index element={<Navigate to="/products" replace/>} />
+              <Route path="/products" element={<ProductsPage/>}/>
+              <Route path="/products/:id" element={<DetailsPage/>}/>
+              <Route path="/checkout" element={<CheckoutPage/>}/>
+              <Route path="/*" element={<PageNotFound/>}/>
+            </Routes>
+          </Layout>
+        </ProductsProvider>
+      </CartProvider>
+    
+     
   )
 }
 
